9b5338d88fc5a15074acd31697fa4c0196a047cb
ELbyFross <vp.kh.ua@gmail.com>
2022-05-01 19:00:10 +0300

    Docs+

--- md__doloro__g_d_k__doloro__editor__extension__r_e_a_d_m_e.js
+++ md__doloro__g_d_k__doloro__editor__extension__r_e_a_d_m_e.js
@@ -4,9 +4,11 @@ var md__doloro__g_d_k__doloro__editor__extension__r_e_a_d_m_e =
     [ "Fields", "md__doloro__g_d_k__doloro__editor__extension__r_e_a_d_m_e.html#Editor", null ],
     [ "Small Tools Hub", "deditorext_sth.html", [
       [ "About", "deditorext_sth.html#deditorext_sth-a", null ],
+      [ "How does it work", "deditorext_sth.html#deditorext_sth_work", null ],
       [ "How to..", "deditorext_sth.html#deditorext_sth-ht", [
         [ "Start", "deditorext_sth.html#deditorext_sth-ht-start", null ],
-        [ "Enable \\ Disable plug-in", "deditorext_sth.html#deditorext_sth-ht-state", null ]
+        [ "Enable \\ Disable plug-in", "deditorext_sth.html#deditorext_sth-ht-state", null ],
+        [ "Configure a plug-in", "deditorext_sth.html#deditorext_sth-ht-conf", null ]
       ] ],
       [ "Custom plug-in", "deditorext_sth.html#deditorext_sth-dev", [
         [ "Creation", "deditorext_sth.html#deditorext_sth-dev-class", null ],
@@ -14,22 +16,22 @@ var md__doloro__g_d_k__doloro__editor__extension__r_e_a_d_m_e =
       ] ]
     ] ],
     [ "Project settings utils", "deditorext_projsetutil.html", [
-      [ "Project settings utils", "deditorext_projsetutil.html#autotoc_md71", null ],
-      [ "Как работает:", "deditorext_projsetutil.html#autotoc_md72", [
-        [ "Tools > Project Setting", "deditorext_projsetutil.html#autotoc_md73", null ]
+      [ "Project settings utils", "deditorext_projsetutil.html#autotoc_md69", null ],
+      [ "Как работает:", "deditorext_projsetutil.html#autotoc_md70", [
+        [ "Tools > Project Setting", "deditorext_projsetutil.html#autotoc_md71", null ]
       ] ],
-      [ "Script:", "deditorext_projsetutil.html#autotoc_md74", [
-        [ "PrjSt", "deditorext_projsetutil.html#autotoc_md75", null ],
-        [ "CreatePrjSt", "deditorext_projsetutil.html#autotoc_md76", null ],
-        [ "ProjectSetting", "deditorext_projsetutil.html#autotoc_md77", null ],
-        [ "LayersSetting", "deditorext_projsetutil.html#autotoc_md78", null ],
-        [ "TagsSetting", "deditorext_projsetutil.html#autotoc_md79", null ],
-        [ "InputSetting", "deditorext_projsetutil.html#autotoc_md80", null ],
-        [ "InputAxis", "deditorext_projsetutil.html#autotoc_md81", null ]
+      [ "Script:", "deditorext_projsetutil.html#autotoc_md72", [
+        [ "PrjSt", "deditorext_projsetutil.html#autotoc_md73", null ],
+        [ "CreatePrjSt", "deditorext_projsetutil.html#autotoc_md74", null ],
+        [ "ProjectSetting", "deditorext_projsetutil.html#autotoc_md75", null ],
+        [ "LayersSetting", "deditorext_projsetutil.html#autotoc_md76", null ],
+        [ "TagsSetting", "deditorext_projsetutil.html#autotoc_md77", null ],
+        [ "InputSetting", "deditorext_projsetutil.html#autotoc_md78", null ],
+        [ "InputAxis", "deditorext_projsetutil.html#autotoc_md79", null ]
       ] ],
-      [ "Editor:", "deditorext_projsetutil.html#autotoc_md82", [
-        [ "ProjectSettingsNew", "deditorext_projsetutil.html#autotoc_md83", null ],
-        [ "ProjectSettingsWindow", "deditorext_projsetutil.html#autotoc_md84", null ]
+      [ "Editor:", "deditorext_projsetutil.html#autotoc_md80", [
+        [ "ProjectSettingsNew", "deditorext_projsetutil.html#autotoc_md81", null ],
+        [ "ProjectSettingsWindow", "deditorext_projsetutil.html#autotoc_md82", null ]
       ] ]
     ] ]
 ];
--- navtreedata.js
+++ navtreedata.js
@@ -96,7 +96,7 @@ var NAVTREE =
     [ "Doloro Tools", "md__doloro__g_d_k__doloro__tools__r_e_a_d_m_e.html", "md__doloro__g_d_k__doloro__tools__r_e_a_d_m_e" ],
     [ "Doloro UI 2", "md__doloro__g_d_k__doloro__u_i_2__r_e_a_d_m_e.html", "md__doloro__g_d_k__doloro__u_i_2__r_e_a_d_m_e" ],
     [ "Changelog", "md__doloro__g_d_k__c_h_a_n_g_e_l_o_g.html", [
-      [ "v2022.1.0b", "md__doloro__g_d_k__c_h_a_n_g_e_l_o_g.html#autotoc_md146", null ]
+      [ "v2022.1.0b", "md__doloro__g_d_k__c_h_a_n_g_e_l_o_g.html#autotoc_md143", null ]
     ] ],
     [ "Todo List", "todo.html", null ],
     [ "Packages", "namespaces.html", [
@@ -186,10 +186,10 @@ var NAVTREEINDEX =
 "class_doloro_1_1_u_i_engine2_1_1_window_state_controller.html#a17eba9695e7f890051cd51265d9b7369",
 "class_doloro_1_1_u_i_engine2_1_1_window_u_i_controller.html#a5ee12fe0cca9b735b4caee189c16f723",
 "dai_core_gs_asm_inst.html#dai_core_gs_asm_inst_rt",
-"dsensors-env-filters.html",
-"interface_doloro_1_1_invetory_system_1_1_i_reservation_agent.html#a4c4fcbc0bb71d010f06e57c66989dd1c",
-"namespace_doloro_1_1_u_i_engine2.html#ac266274ade07a6b45fb2848285d4ff23",
-"struct_doloro_1_1_invetory_system_1_1_properties_1_1_volumetric_space_1_1_coordinate.html#a461b7f1081f782a1432637fb4ca36303"
+"dinv_volume.html#dinv_volume_htu",
+"interface_doloro_1_1_invetory_system_1_1_i_reservation_agent.html",
+"namespace_doloro_1_1_u_i_engine2.html#abfca1f5d63c8debd8f5712d1a8056ab5a1dbcd970a876c8caf66ed6d72378266e",
+"struct_doloro_1_1_invetory_system_1_1_properties_1_1_volumetric_space_1_1_coordinate.html#a0e5c15ff0e4cf15f8e472859973d0d0d"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
