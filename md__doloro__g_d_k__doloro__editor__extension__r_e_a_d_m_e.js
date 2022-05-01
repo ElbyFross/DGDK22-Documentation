babb9a4009fc115c85df874d7439fd03a6700463
ELbyFross <vp.kh.ua@gmail.com>
2022-05-01 16:59:44 +0300

    DOCS+

--- md__doloro__g_d_k__doloro__editor__extension__r_e_a_d_m_e.js
+++ md__doloro__g_d_k__doloro__editor__extension__r_e_a_d_m_e.js
@@ -14,22 +14,22 @@ var md__doloro__g_d_k__doloro__editor__extension__r_e_a_d_m_e =
       ] ]
     ] ],
     [ "Project settings utils", "deditorext_projsetutil.html", [
-      [ "Project settings utils", "deditorext_projsetutil.html#autotoc_md65", null ],
-      [ "Как работает:", "deditorext_projsetutil.html#autotoc_md66", [
-        [ "Tools > Project Setting", "deditorext_projsetutil.html#autotoc_md67", null ]
+      [ "Project settings utils", "deditorext_projsetutil.html#autotoc_md71", null ],
+      [ "Как работает:", "deditorext_projsetutil.html#autotoc_md72", [
+        [ "Tools > Project Setting", "deditorext_projsetutil.html#autotoc_md73", null ]
       ] ],
-      [ "Script:", "deditorext_projsetutil.html#autotoc_md68", [
-        [ "PrjSt", "deditorext_projsetutil.html#autotoc_md69", null ],
-        [ "CreatePrjSt", "deditorext_projsetutil.html#autotoc_md70", null ],
-        [ "ProjectSetting", "deditorext_projsetutil.html#autotoc_md71", null ],
-        [ "LayersSetting", "deditorext_projsetutil.html#autotoc_md72", null ],
-        [ "TagsSetting", "deditorext_projsetutil.html#autotoc_md73", null ],
-        [ "InputSetting", "deditorext_projsetutil.html#autotoc_md74", null ],
-        [ "InputAxis", "deditorext_projsetutil.html#autotoc_md75", null ]
+      [ "Script:", "deditorext_projsetutil.html#autotoc_md74", [
+        [ "PrjSt", "deditorext_projsetutil.html#autotoc_md75", null ],
+        [ "CreatePrjSt", "deditorext_projsetutil.html#autotoc_md76", null ],
+        [ "ProjectSetting", "deditorext_projsetutil.html#autotoc_md77", null ],
+        [ "LayersSetting", "deditorext_projsetutil.html#autotoc_md78", null ],
+        [ "TagsSetting", "deditorext_projsetutil.html#autotoc_md79", null ],
+        [ "InputSetting", "deditorext_projsetutil.html#autotoc_md80", null ],
+        [ "InputAxis", "deditorext_projsetutil.html#autotoc_md81", null ]
       ] ],
-      [ "Editor:", "deditorext_projsetutil.html#autotoc_md76", [
-        [ "ProjectSettingsNew", "deditorext_projsetutil.html#autotoc_md77", null ],
-        [ "ProjectSettingsWindow", "deditorext_projsetutil.html#autotoc_md78", null ]
+      [ "Editor:", "deditorext_projsetutil.html#autotoc_md82", [
+        [ "ProjectSettingsNew", "deditorext_projsetutil.html#autotoc_md83", null ],
+        [ "ProjectSettingsWindow", "deditorext_projsetutil.html#autotoc_md84", null ]
       ] ]
     ] ]
 ];
