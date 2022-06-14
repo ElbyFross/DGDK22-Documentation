ce156fc0b56336e9be0afb5a582991413439476d
ElbyFross <51989477+ElbyFross@users.noreply.github.com>
2022-06-14 20:33:28 +0300

    Docs+

--- md__d___work__git_hub__d_g_d_k22__assets__tauri__interactive__doloro__g_d_k__doloro__sensor__r_e_a_d_m_e.js
+++ md__d___work__git_hub__d_g_d_k22__assets__tauri__interactive__doloro__g_d_k__doloro__sensor__r_e_a_d_m_e.js
@@ -8,12 +8,16 @@ var md__d___work__git_hub__d_g_d_k22__assets__tauri__interactive__doloro__g_d_k_
         [ "Filtered Sensor", "dsensors-env.html#dsensors-env-types-fsensor", null ],
         [ "Dependent Filtered Sensor", "dsensors-env.html#dsensors-env-types-dfsensor", null ]
       ] ],
-      [ "Example", "dsensors-env.html#dsensors-env-example", null ]
+      [ "Sensors layout example", "dsensors-env.html#dsensors-env-example", null ]
     ] ],
     [ "Sensor filters", "dsensors-env-filters.html", [
       [ "About", "dsensors-env-filters.html#dsensors-env-filters-a", null ],
       [ "How to use", "dsensors-env-filters.html#dsensors-env-filters-htu", null ],
       [ "Included filters", "dsensors-env-filters.html#dsensors-env-filters-bf", null ]
     ] ],
-    [ "Filters development", "dsensors-env-filter-new.html", null ]
+    [ "Filters development", "dsensors-env-filter-new.html", [
+      [ "About", "dsensors-env-filter-new.html#dsensors-env-filter-new-a", null ],
+      [ "New filter creation", "dsensors-env-filter-new.html#dsensors-env-filter-new-creation", null ],
+      [ "Development rules", "dsensors-env-filter-new.html#dsensors-env-filter-new-rules", null ]
+    ] ]
 ];
--- md__d___work__git_hub__d_g_d_k22__assets__tauri__interactive__doloro__g_d_k__doloro__u_i_2__r_e_a_d_m_e.js
+++ md__d___work__git_hub__d_g_d_k22__assets__tauri__interactive__doloro__g_d_k__doloro__u_i_2__r_e_a_d_m_e.js
@@ -1,12 +1,21 @@
 var md__d___work__git_hub__d_g_d_k22__assets__tauri__interactive__doloro__g_d_k__doloro__u_i_2__r_e_a_d_m_e =
 [
-    [ "Custom Controls", "dui2-controls.html", "dui2-controls" ],
+    [ "About", "md__d___work__git_hub__d_g_d_k22__assets__tauri__interactive__doloro__g_d_k__doloro__u_i_2__r_e_a_d_m_e.html#dui2-a", null ],
+    [ "Modules", "md__d___work__git_hub__d_g_d_k22__assets__tauri__interactive__doloro__g_d_k__doloro__u_i_2__r_e_a_d_m_e.html#sec_dui2_catalog", null ],
+    [ "Custom Controls", "dui2-controls.html", [
+      [ "About", "dui2-controls.html#dui2-controls-a", null ],
+      [ "Core classes", "dui2-controls.html#dui2-controls-core", [
+        [ "AGUIElement", "dui2-controls.html#dui2-controls-core-element", null ],
+        [ "AGUIField", "dui2-controls.html#dui2-controls-core-field", null ]
+      ] ],
+      [ "Implemented controls", "dui2-controls.html#dui2-controls-impl", null ]
+    ] ],
+    [ "Window", "dui2-window.html", "dui2-window" ],
     [ "Runtime Workspace", "dui2-rwl.html", "dui2-rwl" ],
     [ "Virtual UI Hub", "dui2-vh.html", null ],
     [ "Color Palettes", "dui2-palette.html", null ],
     [ "Elements Collection", "dui2-el_col.html", null ],
     [ "Context Menu", "dui2-context.html", null ],
-    [ "Window", "dui2-window.html", "dui2-window" ],
     [ "Tabs", "dui2-tabs.html", null ],
     [ "Helpers", "dui2-helpers.html", null ]
 ];
